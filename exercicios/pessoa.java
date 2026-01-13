6ece1a105a0d88caff76320c2dacb131ca3175ee
Ortega <jonasbispo3@gmail.com>
2026-01-13 18:27:15 -0300

    feat: adiciona classe pessoa e uso em HelloWorld

---
+++ exercicios/pessoa.java
@@ -0,0 +1,22 @@
+public class pessoa {
+    //Atributos (dados)
+    String nome;
+    int idade;
+    double altura;
+
+    //Construtor
+    public pessoa(String nome, int idade, double altura) {
+       this.nome = nome;
+       this.idade = idade;
+       this.altura = altura;
+    }
+
+    // Método
+     public void apresentar(){
+        System.out.println("nome: " + nome);
+        System.out.println("idade: " + idade);
+        System.out.println("altura: " + altura);
+     }
+
+
+}
